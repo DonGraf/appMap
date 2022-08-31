d768bef136c62ba4188d332d9f8000c2bb4556d3
DonGraf <f.perez.pincheira@gmail.com>
2022-08-31 13:27:41 -0400

    Menu lateral con sus subdirecciones agregada

--- src/app/mapas/mapas-routing.module.ts
+++ src/app/mapas/mapas-routing.module.ts
@@ -10,11 +10,11 @@ const routes: Routes = [
   {
     path: '',
     children: [
-      { path:'fullscreem', component:FullScreenComponent},
+      { path:'fullscreen', component:FullScreenComponent},
       { path:'zoom-range', component:ZoomRangeComponent},
       { path:'marcadores', component:MarcadoresComponent},
       { path:'propiedades', component:PropiedadesComponent},
-      { path:'**', redirectTo:'fullscreem'},
+      { path:'**', redirectTo:'fullscreen'},
     ]
   }
 
