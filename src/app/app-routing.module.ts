b220fb1d2670e4146c13a2f78722bfaac7bcf630
DonGraf <f.perez.pincheira@gmail.com>
2022-08-31 12:57:26 -0400

    Componentes y rutas agregadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  //Sistema de rutas principal
+  {
+    path: 'mapas',
+    //carga
+    loadChildren:()=>import('./mapas/mapas.module').then(m=> m.MapasModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'mapas'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/mapas/mapas-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FullScreenComponent } from './pages/full-screen/full-screen.component';
+import { MarcadoresComponent } from './pages/marcadores/marcadores.component';
+import { PropiedadesComponent } from './pages/propiedades/propiedades.component';
+import { ZoomRangeComponent } from './pages/zoom-range/zoom-range.component';
+
+const routes: Routes = [
+  //lazy load
+  {
+    path: '',
+    children: [
+      { path:'fullscreem', component:FullScreenComponent},
+      { path:'zoom-range', component:ZoomRangeComponent},
+      { path:'marcadores', component:MarcadoresComponent},
+      { path:'propiedades', component:PropiedadesComponent},
+      { path:'**', redirectTo:'fullscreem'},
+    ]
+  }
+
+];
+
+
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class MapasRoutingModule { }
