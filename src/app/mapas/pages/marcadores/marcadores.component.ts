103cc2a3d8e1025dc0ff67428e48c7767cd0756f
DonGraf <f.perez.pincheira@gmail.com>
2022-08-31 23:32:08 -0400

    Marcadores con movimiento y colores agregados

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -1,16 +1,76 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-marcadores',
   templateUrl: './marcadores.component.html',
   styles: [
+    `
+    .mapa-container{
+      height: 100%;
+      width: 100%;
+    }
+    .list-group {
+      position: fixed;
+      top: 20px;
+      right: 20px;
+      z-index: 99;
+    }
+    li{
+      cursor:pointer;
+    }
+    `
   ]
 })
-export class MarcadoresComponent implements OnInit {
+export class MarcadoresComponent implements AfterViewInit {
 
+  //Conecta el #mapa del zoom-range.html
+  @ViewChild('mapa') divMapa!: ElementRef
+  mapa !: mapboxgl.Map;
+  zoomLevel: number = 14;
+  center: [number,number] = [-71.958331,-36.434009]
+
+  
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.center,
+      zoom: this.zoomLevel
+    });
+  
+    /*
+    Coloca un texto en vez del marcador
+    -----------------------------------
+    const markerHtml: HTMLElement = document.createElement('div');
+    markerHtml.innerHTML = "DonGraffth";
+    // Referencia del marcador
+    const marker = new mapboxgl.Marker({
+      element: markerHtml
+    }).*/
+
+    /** Marcador de manera estatica 
+    const marker = new mapboxgl.Marker().
+    // La posicion del marcador
+    setLngLat(this.center).
+    //Se agrega el marcador al mapa
+    addTo(this.mapa);
+    */
+  }
+
+  irMarcador(){
+
+  }
+
+  addMarcador(){
+    const colorMarcadorAleatorio = "#xxxxxx".replace(/x/g, y=>(Math.random()*16|0).toString(16));
+
+    const nuevoMarcador = new mapboxgl.Marker({
+      draggable: true,
+      color: colorMarcadorAleatorio
+    }).setLngLat(this.center).addTo(this.mapa);
   }
 
 }
